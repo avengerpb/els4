e5148b87b865f30cdbd67e93c9f7bc898f935118
avengerpb <datlequoc96@gmail.com>
2016-07-25 17:10:22 +0700

    generate-lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,6 +4,16 @@ class LessonsController < ApplicationController
   def create
     @course = Course.find_by(id: params[:course_id])
     @lesson = Lesson.new(user_id: current_user.id, course_id: @course.id)
-    @lesson.save
+    @check = Lesson.find_by(user_id: current_user.id, course_id: @course.id)
+    if @check.nil?
+      @lesson.save
+      redirect_to course_lesson_path(@course.id, current_user.id)
+    else
+      redirect_to course_lesson_path(@course.id, current_user.id)
+    end
+  end
+
+  def show
+    @words = Word.find_by(course_id: params[:course_id])
   end
 end
